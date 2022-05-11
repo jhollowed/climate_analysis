88743e53431cb9ed75cbe1fae85fe512fdebd590
Joe Hollowed <hollowed@umich.edu>
2022-05-10 21:01:39 -0700

    changes to create case for 30 year E3SM AOA run

--- CLDERA/HSW/hsw_case_builder/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./create_FIDEAL_case_ne16L72.sh 1 192
-./create_FIDEAL_case_ne16L72.sh 1 384
-./create_FIDEAL_case_ne16L72.sh 1 768
-./create_FIDEAL_case_ne16L72.sh 1 1536
-./create_FIDEAL_case_ne16L72.sh 1 3072
-./create_FIDEAL_case_ne16L72.sh 1 16384
