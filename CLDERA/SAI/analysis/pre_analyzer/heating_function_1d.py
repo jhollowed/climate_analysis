1f5c9cc8b072e29e504cb6c92a925e2336f58cbe
Joe Hollowed <hollowed@umich.edu>
2022-11-22 20:30:54 -0800

    oops

--- CLDERA/SAI/analysis/pre_analyzer/heating_function_1d.py
+++ CLDERA/SAI/analysis/pre_analyzer/heating_function_1d.py
@@ -9,10 +9,11 @@ import pdb
 dTstrat = 0.3 * u.K/u.day
 qstar = 9.5e-7
 q0 = 1e-12
-qplotmax = -4
+qplotmax = 0
+dTplotmax = 1
 #pp = np.abs(-12 - np.log10(qstar))
 q = np.logspace(-12, qplotmax, 100)
-gamma = [8, 10, 12, 14, 16]
+gamma = [8, 10, 12]
 
 # ---- linear form
 s = (q/qstar * const.Cp_d * dTstrat).to(u.J/u.kg/u.s)
@@ -28,13 +29,39 @@ for i in range(len(gamma)):
     sdd = (s / const.Cp_d).to(u.K/u.day)
     plt.plot(q, sdd, label='gamma={}'.format(gamma[i]))
 
+#--- sigmoid form
+k = [1, 2, 3, 4, 5]
+for i in range(len(k)):
+    sig = lambda qq: 2 / (1 + np.exp(-k[i]*np.log10(qq/qstar)))
+    dsig = lambda qq: \
+           k[i]*2*np.exp(-k[i]*np.log10(qq/qstar)) / (1 + np.exp(-k[i]*np.log10(qq/qstar)))**2
+    s = sig(q)
+    #s[q > qstar] = ((dsig(qstar)) * np.log10(q/qstar) + 1)[q>qstar]
+    #s = s * np.abs((np.log10(qstar) - 7)/(np.log10(q) - 2))
+    #s = s + 0.5 * np.log10(q)-np.log10(qstar)
+    s = (s * dTstrat * const.Cp_d).to(u.J/u.kg/u.s)
+    sddd = (s / const.Cp_d).to(u.K/u.day)
+    plt.plot(q, sddd, '--', label='k={}'.format(k[i]))
+
+#--- asinh form
+#b = [6, 8, 100]
+#q0 = 1e-10
+#for i in range(len(b)):
+#    s = (dTstrat * (1 - (np.arcsinh(b[i]*np.log10(qstar/q)) / np.arcsinh(b[i]*np.log10(qstar/q0)))) \
+#         * np.abs(np.log10(qstar)/np.log10(q)) * const.Cp_d).to(u.J/u.kg/u.s)
+#    sdddd = (s / const.Cp_d).to(u.K/u.day)
+#    plt.plot(q, sdddd, ':', label='b={}'.format(b[i]))
+
 plt.xscale('log')
 plt.plot([qstar, qstar], plt.ylim(), '--k', lw=0.75)
 plt.xlabel('mixing ratio')
 plt.ylabel('local heating rate [K/day]')
+plt.ylim([-0.2, dTplotmax])
 plt.grid()
 plt.legend()
 
+# tmp
+plt.show()
 
 # ---------- aod cooling
 
